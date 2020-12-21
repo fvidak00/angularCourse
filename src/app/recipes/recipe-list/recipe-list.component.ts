62b6d286b8a9db8b5baa31b0962a41fb664d9a40
fvidak <fvidak00@fesb.hr>
2020-12-21 15:57:39 +0100

    Added dummy items in recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes=[]
+  recipes:Recipe[]=[
+    new Recipe(
+      "Test recipe",
+      "test",
+      "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg"
+    ),
+    new Recipe(
+      "Test recipe 2",
+      "test 2",
+      "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg"
+    )
+  ]
 
   constructor() { }
 
